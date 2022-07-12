40d12334107f3edad726f352793e6c6fd3e8fff6
Silicon-Melter <68177054+Silicon-Melter@users.noreply.github.com>
2022-07-12 19:01:20 +0700

    again

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import random
 import os
-from re import A
+from re import A,T
 import numpy as np
 import librosa
 import librosa.display
@@ -21,6 +21,13 @@ import array
 import math
 import wave
 import cloudconvert
+from signal import Signals
+from statistics import mean
+import sys
+from scipy.signal import find_peaks
+from urllib.parse import _ParseResultBase
+from pydub import AudioSegment
+
 
 app = Flask(__name__)
 
@@ -60,151 +67,21 @@ def spectogram(file_name, user_uid):
                                 y_axis=y_axis,
                                 cmap='viridis')
         plot.colorbar(format="%+2.f")
-        plot.savefig(file_name.replace('.mp3', '')+'.png', transparent=True)
+        plot.savefig(file_name.replace('.wav', '')+'.png', transparent=True)
     Y_scale = np.abs(S_scale) ** 2
     Y_log_scale = librosa.power_to_db(Y_scale)
     plot_spectrogram(Y_log_scale, sr, HOP_SIZE, y_axis="log")
 
     # firebase
-    picture = (file_name.replace('.mp3', '')+'.png')
+    picture = (file_name.replace('.wav', '')+'.png')
     storage.child(picture).put(picture)
-    url = storage.child(picture).get_url('')
+    url = storage.child(picture).get_url(user_uid)
     doccument = db.collection('result').document(user_uid).collection(
-        "user_result").document(file_name.replace('.mp3', ''))
+        "user_result").document(file_name.replace('.wav', ''))
     doccument.update({'spectogram': url})
 
-
-def read_wav(filename):
-    # open file, get metadata for audio
-    try:
-        wf = wave.open(filename, "rb")
-    except IOError as e:
-        print(e)
-        return
-
-    # typ = choose_type( wf.getsampwidth() ) # TODO: implement choose_type
-    nsamps = wf.getnframes()
-    assert nsamps > 0
-
-    fs = wf.getframerate()
-    assert fs > 0
-
-    # Read entire file and make into an array
-    samps = list(array.array("i", wf.readframes(nsamps)))
-
-    try:
-        assert nsamps == len(samps)
-    except AssertionError:
-        print(nsamps, "not equal to", len(samps))
-
-    return samps, fs
-
-
-def peak_detect(data):
-    max_val = np.amax(abs(data))
-    peak_ndx = np.where(data == max_val)
-    if len(peak_ndx[0]) == 0:  # if nothing found then the max must be negative
-        peak_ndx = np.where(data == -max_val)
-    return peak_ndx
-
-
-def bpm_detector(data, fs):
-    cA = []
-    cD = []
-    correl = []
-    cD_sum = []
-    levels = 4
-    max_decimation = 2 ** (levels - 1)
-    min_ndx = math.floor(60.0 / 300 * (fs / max_decimation))
-    max_ndx = math.floor(60.0 / 40 * (fs / max_decimation))
-
-    for loop in range(0, levels):
-        cD = []
-        # 1) DWT
-        if loop == 0:
-            [cA, cD] = pywt.dwt(data, "db4")
-            cD_minlen = len(cD) / max_decimation + 1
-            cD_sum = np.zeros(math.floor(cD_minlen))
-        else:
-            [cA, cD] = pywt.dwt(cA, "db4")
-
-        # 2) Filter
-        cD = signal.lfilter([0.01], [1 - 0.99], cD)
-
-        # 4) Subtract out the mean.
-
-        # 5) Decimate for reconstruction later.
-        cD = abs(cD[:: (2 ** (levels - loop - 1))])
-        cD = cD - np.mean(cD)
-
-        # 6) Recombine the signal before ACF
-        #    Essentially, each level the detail coefs (i.e. the HPF values) are concatenated to the beginning of the array
-        cD_sum = cD[0: math.floor(cD_minlen)] + cD_sum
-
-    # Adding in the approximate data as well...
-    cA = signal.lfilter([0.01], [1 - 0.99], cA)
-    cA = abs(cA)
-    cA = cA - np.mean(cA)
-    cD_sum = cA[0: math.floor(cD_minlen)] + cD_sum
-
-    # ACF
-    correl = np.correlate(cD_sum, cD_sum, "full")
-
-    midpoint = math.floor(len(correl) / 2)
-    correl_midpoint_tmp = correl[midpoint:]
-    peak_ndx = peak_detect(correl_midpoint_tmp[min_ndx:max_ndx])
-
-    peak_ndx_adjusted = peak_ndx[0] + min_ndx
-    bpm = 60.0 / peak_ndx_adjusted * (fs / max_decimation)
-    print(bpm)
-    return bpm, correl
-
-
-def main_bpm(url,user_uid,file_name):
-    filename = convert(url)
-    samps, fs = read_wav(filename)
-    data = []
-    correl = []
-    bpm = 0
-    n = 0
-    nsamps = len(samps)
-    window_samps = int(3 * fs)
-    samps_ndx = 0  # First sample in window_ndx
-    max_window_ndx = math.floor(nsamps / window_samps)
-    bpms = np.zeros(max_window_ndx)
-
-    # Iterate through all windows
-    for window_ndx in range(0, max_window_ndx):
-
-        # Get a new set of samples
-        # print(n,":",len(bpms),":",max_window_ndx_int,":",fs,":",nsamps,":",samps_ndx)
-        data = samps[samps_ndx : samps_ndx + window_samps]
-        if not ((len(data) % window_samps) == 0):
-            raise AssertionError(str(len(data)))
-
-        bpm, correl_temp = bpm_detector(data, fs)
-        if bpm is None:
-            continue
-        bpms[window_ndx] = bpm
-        correl = correl_temp
-
-        # Iterate at the end of the loop
-        samps_ndx = samps_ndx + window_samps
-
-        # Counter for debug...
-        n = n + 1
-
-    bpm = np.median(bpms)
-    realbpm = bpm/2
-    doccument = db.collection('result').document(user_uid).collection(
-        "user_result").document(file_name.replace('.mp3', ''))
-    doccument.update({'bpm': realbpm})
-    print(bpm/2)
-    return(bpm/2)
-
 def convert(url):
-    api = cloudconvert.configure(api_key='eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNDZmMDYzNWUyYTRmYmRkZWE3OGQ2Yzc4OTNjYjQyZTBjMTY2ODBmNDc3NGUzMzYyMDEyYjRmNTNlYTFkMTRlODExNGM4YzM1ZjcyYjljNDQiLCJpYXQiOjE2NDU2OTkyNDkuMTEwNDY5LCJuYmYiOjE2NDU2OTkyNDkuMTEwNDcxLCJleHAiOjQ4MDEzNzI4NDkuMTA2MjkzLCJzdWIiOiI1NjQyNjMzNyIsInNjb3BlcyI6WyJ1c2VyLnJlYWQiLCJ1c2VyLndyaXRlIiwidGFzay5yZWFkIiwidGFzay53cml0ZSIsIndlYmhvb2sucmVhZCIsIndlYmhvb2sud3JpdGUiLCJwcmVzZXQucmVhZCIsInByZXNldC53cml0ZSJdfQ.IQUZ47Iwy08mXjfEs4EtACseQ4VCVFFYfOXKg7muRNRqDcdLykU5NlHCyvM74DoBcGx8UDexIh2BkpD7WVCN6Jez5spif7xuTRdRisC-fWODQBkqj_Vz4qbREi_lSa6ai_YUL3X7pHz7xYQIwAo2WzSiEls-N1k405fgQ0-oXAsG4i3ZxyPjeEZrT1Y7_2FptoDk26bArm6ebWe5QV0AHix5frJf6dyj44TBTKsrHiB_rTx_h8_xmyY1fzycfibmCsC2SLQGeuwggaIdQwqZdc3aLALdlnlYoGS9CALQ9seSWn48bJsbvr66PHllWUb3bFkrGo1u4DoTLhF1U03nMy01OmO4pf8fKXhmOMSAjAlzEmFqOPVYf9ZH8zATHO899TLxWhvVKMPrNaGUstqvMyQ0d9nvrvly2w9L1mTE9q6yNBxJGNM5Ptz4s724KNjhsReHamWOJQ4BiF_MipP5AgDO7RDV2k4pLS4ZEWewKSbJp6JkSLRXRCl13Gai8v8T3cqjj9eq97pW999JHcy5uEc9prXpM8iiNMK1_AVzvsXJASwe28OEXweN7QfuqBVajGotUERQ32OH4TesVdu2OwNCqgLuV_56CHQwg438QH1HXdk_xLSUU7f9Q3pReUSiDmhEwRzQR7n0zHJEha0CFmcu5msqUOzcA9rfoDXzxH8')
-
+    api = cloudconvert.configure(api_key='eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYmRiNDJmMWQ5YzQyNzYzNWMxODY3Y2ZiYjkyNjQwOWVkNjQyNjljZTNlOGY1NTQ1ODBjYjE3NTMwNzY5MTQ4YzFlMDc3ZWZiZWY3OTQyZGIiLCJpYXQiOjE2NTQ3NDk1NTEuNzQxOTM5LCJuYmYiOjE2NTQ3NDk1NTEuNzQxOTQxLCJleHAiOjQ4MTA0MjMxNTEuNzM1NTQ3LCJzdWIiOiI1NTk2NDUyNiIsInNjb3BlcyI6WyJ1c2VyLndyaXRlIiwidXNlci5yZWFkIiwidGFzay5yZWFkIiwidGFzay53cml0ZSIsIndlYmhvb2sucmVhZCIsIndlYmhvb2sud3JpdGUiLCJwcmVzZXQucmVhZCIsInByZXNldC53cml0ZSJdfQ.cFYOIuJLDNypMGga-yVBSrpLHu85Xq7pHBguPy9bm5W4Fi-ylYTU-bWBo1QuSSC-_z-4hq_rd_bz0D7V5GCLnypnQ-DXgEYopfnWRUueVBre78Qtz1K2oHKbfYxOj4L0aPJ1CaDl_0DJtWkAS-0jgpz5L5V0-Sd7KxxlghcllNQIFSz6x4fufnXCNIS0eoD6tZ7jEbjrRhVCY72hZ-kvElUwUBeCeoLrb1pQHTmzdJo1AUJlogqoygczVVvUFvPD9IyDmvBuVlPG2WeoiE2kDbmFaPqi2Vu4VHLWHPeU275CHL8JrsHzEFgfnJI0LMUpaJYdYA-kJZQpMTox1QkHVcult6e8lF_OYb7nrPLBIHphCFvnHL5ufFF2Z63OVdUuKS30l167MlvKUpY0uGcmIPVLZj76B2AMv4URAfYLm_CE7V9C6Rc0WUa8nemBljnR48IdV6NTjLuG5N3xX4fzVEEJUV8nv90vhwzb5iPIjR2zSRKp2k_2iCuADL9Rs-YNO_-t7rgCTknrkClzOL_78Sf5RkYXflBMJbAyGadTZ4nS-a-Jmzgs3SCyDFpVLOfXX2Wwys3lQP0URm_QD7z12qeBWXShnR2t_fvk3ZPcRFbkjyVfNZkOUnF8a9xay0BR2I8WNw-16NljQVByi1dUMNoEEvNvRcoD2O6OBPrvXM0')
     job = cloudconvert.Job.create(payload={
         "tasks": {
             'import-my-file': {
@@ -232,7 +109,78 @@ def convert(url):
     file = res.get("result").get("files")[0]
     res = cloudconvert.download(filename=file['filename'], url=file['url'])
     print(res)
-    return(res) 
+    print(file)
+    return(res)
+
+def countbpm(filename, user_uid):
+    audioname = filename
+
+    spf = wave.open(audioname, "r")
+
+    # Extract Raw Audio from Wav File
+    signal = spf.readframes(-1)
+    signal = np.fromstring(signal, dtype="Int16")
+    fs = spf.getframerate()
+
+    Time = np.linspace(0, len(signal) / fs, num=len(signal))
+
+    #Find peaks
+    peaks = find_peaks(signal, height = 10000, threshold = 1000, distance = 1000)
+    height = peaks[1]['peak_heights'] #list of the heights of the peaks
+    peak_pos = Time[peaks[0]] #list of the peaks positions
+
+    #Finding the minima
+    y2 = signal*-1
+    minima = find_peaks(y2)
+    min_pos = Time[minima[0]] #list of the minima positions
+    min_height = y2[minima[0]] #list of the mirrored minima heights
+    # print(signal)
+    i = 0
+    """ Status = True;
+    lowP=[] 
+    for signals in signal:
+        if(signals < 100 and Status == True and signals > 0):
+            i = i+1
+            print(signals)
+            Status = False
+            lowP.append(signals)
+        elif(signals > 15000):
+            Status = True
+    TimelowP = np.linspace(0, len(lowP) / fs, num=len(lowP))
+    """
+    signalAVG =[]
+    signalsSUM = 0
+
+    for signals in signal:
+        if(i < 332):
+            signalsSUM = signalsSUM + signals
+        elif(i >= 332):
+            signalAVG.append(signalsSUM/333)
+            i = 0
+            signalsSUM = 0
+        i = i+1
+    TimeAVG = np.linspace(0, len(signalAVG) / fs, num=len(signalAVG))
+    # print(signalAVG)
+    # print(mean(signalAVG))
+    print(len(signalAVG))
+
+    maxSignalAVG = int(max(signalAVG)/4)
+    print(maxSignalAVG)
+    #Find peaks
+    peaksAVG = find_peaks(signalAVG, height = maxSignalAVG, threshold = 10, distance = 10)
+    heightAVG = peaksAVG[1]['peak_heights'] #list of the heights of the peaks
+    peak_posAVG = TimeAVG[peaksAVG[0]] #list of the peaks positions
+    startbpm = ((len(peak_posAVG))/2)*6
+    x, sr = librosa.load(audioname) 
+    ipd.Audio(x, rate=sr)
+    print(x)
+    tempo, beat_times = librosa.beat.beat_track(x, sr=sr, start_bpm=startbpm, units='time')
+    print(tempo)
+    print(beat_times)
+    doccument = db.collection('result').document(user_uid).collection(
+        "user_result").document(filename.replace('.wav', ''))
+    doccument.update({'bpm': tempo})
+    return(tempo)
 
 filelist = [f for f in os.listdir(".") if f.endswith(
     ".mp3") or f.endswith(".wav") or f.endswith(".png")]
@@ -247,43 +195,39 @@ config = {
     "storageBucket": "mypros-283015.appspot.com",
     "serviceAccount": "serviceAccountKey.json"
 }
-cred = credentials.Certificate("mypros-283015-firebase-adminsdk-eewnd-fa5c258fcf.json")
+cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 firebase_storage = pyrebase.initialize_app(config)
 
 db = firestore.client()
 storage = firebase_storage.storage()
-
-af = load_model('AF.h5')
+auth = firebase_storage.auth()
+email = "noppawitearth123456@gmail.com"
+password = "123456"
+user = auth.sign_in_with_email_and_password(email, password)
+af = load_model('af_fa.h5')
 brady = load_model('brady.h5')
-murmur = load_model('murmur.h5')
+murmur = load_model('new_murmur.h5')
 
 app = Flask(__name__)
 
 
-@app.route("/text", methods=["POST"])
+@app.route("/text", methods=["GET"])
 def text():
     name_file = request.values["name"]
     return('hello' + name_file)
 
 
-@app.route("/heart", methods=["POST"])
+@app.route("/heart", methods=["GET"])
 def audio():
     data = []
-    try:
-        all_files = storage.list_files()
-        for file in all_files:
-            print(file.name)
-            file.download_to_filename(file.name)
-    except:
-        print("An exception occurred")
-    h_sound = request.values["heart"]
     user_uid = request.values["uid"]
     url = request.values["url"]
     print(user_uid)
+    h_sound = convert(url)
     test1 = extract_data(h_sound)
     spectogram(h_sound, user_uid)
-    main_bpm(url,user_uid,h_sound)
+    bpm = countbpm(h_sound, user_uid)
     data.append(test1)
     af_result = af.predict(np.array(data))
     brady_result = brady.predict(np.array(data))
@@ -294,7 +238,7 @@ def audio():
     af_return = y[0]*100
     brady_return = a[0]*100
     murmur_return = b[0]*100
-    returnvalue = [af_return, brady_return, murmur_return]
+    returnvalue = [af_return, brady_return, murmur_return, bpm]
     return(str(returnvalue))
 
 
